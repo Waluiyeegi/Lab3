45ece8406d4ac0ac88b586ea855049ba059e80dd
Caleb Lui-Yee <cluiyeec@gmail.com>
2022-09-18 19:50:13 -0400

    Added Joe, Bob, and Buddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -25,6 +25,24 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("AddressBook test");
+
+        BuddyInfo joe = new BuddyInfo("Joe", "123 Maple Street", 12345678);
+        BuddyInfo bob = new BuddyInfo("Bob", "131 Maple Street", 12359049);
+        AddressBook book = new AddressBook();
+        System.out.println("The Address Book currently has " + book.getListSize() + " items.");
+        book.addBuddy(joe);
+        System.out.println("The Address Book currently has " + book.getListSize() + " items.");
+        book.addBuddy(bob);
+        System.out.println("The Address Book currently has " + book.getListSize() + " items.");
+        book.removeBuddy(joe);
+        System.out.println("The Address Book currently has " + book.getListSize() + " items.");
+        book.removeBuddy(bob);
+        System.out.println("The Address Book currently has " + book.getListSize() + " items.");
+
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
 }
