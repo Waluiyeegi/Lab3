b3627da62e52f0a6515b0ada7fdd221602ba9925
Caleb Lui-Yee <cluiyeec@gmail.com>
2022-09-26 15:24:52 -0400

    added addressbook test

---
+++ src/BuddyInfo.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class BuddyInfo {
+
+    public BuddyInfo(String name, String address, int phoneNumber){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+    private String name;
+    private String address;
+    private int phoneNumber;
+
+
+    public static void main(String[] args) {
+        BuddyInfo joe = new BuddyInfo("Joe", "123 Maple Street", 12345678);
+        System.out.println("Hi " + joe.getName() + " who lives on " + joe.getAddress() + " and whose phone number is " + joe.getPhoneNumber() + ".");
+
+    }
+}
+
+
+
+
+
